4a15c17efb54c2d9bef21f58afe9e1881b11f480
wx999 <elevenwang@mooyoo.com.cn>
2016-10-24 16:28:10 +0800

    job4

--- app/src/main/java/com/example/sm/myapplication/MainActivity.java
+++ app/src/main/java/com/example/sm/myapplication/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.sm.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//test 新功能呢 哈哈哈
+//test 新功能呢 哈哈哈 job4
 public class MainActivity extends AppCompatActivity {
 
     @Override
